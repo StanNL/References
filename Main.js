a7a7bf1d6a837bb2d1d3c8957383f6fffb126e8d
Stan <stvanbaarsen@gmail.com>
2018-11-14 14:10:44 +0100

    add p5

--- Main.js
+++ Main.js
@@ -1,6 +1,7 @@
 var items = {
 	'jQuery': '&lt;script src=" https://code.jquery.com/jquery-3.3.1.min.js "&gt; &lt;/script&gt;',
-	'Materialize': '&lt;link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css "&gt; \n&lt;script src=" https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js "&gt; &lt;/script&gt;'
+	'Materialize': '&lt;link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css "&gt; \n&lt;script src=" https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js "&gt; &lt;/script&gt;',
+	'p5js': "&lt;script src='https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/p5.min.js' &gt;&lt;/script&gt;"
 }
 
 $(document).ready(function () {
