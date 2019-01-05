fbdca0e6857797a6b821fe4c6644b7222e451dc3
Stan <stvanbaarsen@gmail.com>
2019-01-05 16:22:40 +0100

    improve efficiency, add animations, improve layout, add title, change title, etc.

--- Main.js
+++ Main.js
@@ -1,25 +1,59 @@
-var items = {}
+var items = {};
+var anim1speed = 400;
+var anim2speed = 280;
+var shownTime = 3250;
+var margin = 1.1;
+var lastClick;
 
 $(document).ready(function () {
-	$.getJSON("Items.json", function(data){
+	$.getJSON("Items.json", function (data) {
 		items = data;
-		
+
 		ks = Object.keys(items);
 		for (let i = 0; i < ks.length; i++) {
-			$("<h3>").html(ks[i]).appendTo("#main");
-			$("<pre>").html(items[ks[i]]).appendTo("#main");
+			latest = $("<div class='item'>").appendTo("#main");
+			$("<h3>").html(ks[i]).appendTo(latest);
+			$("<pre>").html(items[ks[i]]).appendTo(latest);
 		}
-	
+
 		dq = $("pre");
 		for (let i = 0; i < dq.length; i++) {
 			dq[i].addEventListener('click', function () {
+				lastClick = +new Date();
 				res.innerHTML = this.innerHTML;
-				M.toast({
-					html: "Code copied to clipboard!",
-					duration: 2500
-				});
 				navigator.clipboard.writeText(res.textContent);
+				$("#toast").addClass("showing");
+				setTimeout(function () {
+					$("#toast").addClass("shown");
+					setTimeout(function () {
+						$("#toastText").addClass("shown");
+					}, anim2speed);
+				}, anim1speed);
+
+				setTimeout(function () {
+					if (checkTime(shownTime)) {
+						$("#toastText").removeClass("shown");
+						setTimeout(function () {
+							if (checkTime(shownTime + anim2speed)) {
+								$("#toast").removeClass("shown");
+								setTimeout(function () {
+									if (checkTime(shownTime + anim2speed + anim1speed)) {
+										$("#toast").removeClass("showing");
+									}
+								}, anim1speed);
+							}
+						}, anim2speed);
+					}
+				}, shownTime);
 			});
 		}
 	})
 });
+
+function tDiff() {
+	return Math.abs(+new Date() - lastClick);
+}
+
+function checkTime(t) {
+	return (tDiff() < (t * margin)) && (tDiff() > (shownTime / margin));
+}
